ccd751b96ff28aa4280d93874ee79df1b9e1f65e
ccnets-team <75417171+junho-deepdream@users.noreply.github.com>
2023-11-04 18:03:39 +0900

    hot fix: rl_params default num_layer increment 4 ->3 after experiments

--- utils/setting/rl_params.py
+++ utils/setting/rl_params.py
@@ -23,7 +23,7 @@ class AlgorithmParameters:
         self.curiosity_factor = curiosity_factor
             
 class NetworkParameters:
-    def __init__(self,  num_layer=3, hidden_size=128, forward_neural_network = GPT, reverse_neural_network = ReverseTransformerDecoder):
+    def __init__(self,  num_layer=4, hidden_size=128, forward_neural_network = GPT, reverse_neural_network = ReverseTransformerDecoder):
         self.num_layer = num_layer
         self.hidden_size = hidden_size
         self.forward_neural_network = forward_neural_network
