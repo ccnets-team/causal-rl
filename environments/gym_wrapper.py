Fix Reward Sync Issue in OpenAI Gym: Adjust Ongoing Reward Calculation in env.step()

environments/gym_wrapper.py
@@ -130,13 +130,13 @@ class GymEnvWrapper(AgentExperienceCollector):
         self.agent_reset[done] = True 
 
         if not self.test_env:
-            self.update_for_training(done, ongoing_terminated, info, action, ongoing_next_obs)
+            self.update_for_training(done, ongoing_terminated, info, action, ongoing_next_obs, ongoing_reward)
         else:
             self.update_for_test(done, action, ongoing_next_obs, ongoing_reward)
         
         return False
 
-    def update_for_training(self, done: np.ndarray, ongoing_terminated: np.ndarray, info, action, ongoing_next_obs: np.ndarray):
+    def update_for_training(self, done: np.ndarray, ongoing_terminated: np.ndarray, info, action, ongoing_next_obs: np.ndarray, ongoing_reward: np.ndarray):
         """
         Processes the information for training update.
         
@@ -148,6 +148,8 @@ class GymEnvWrapper(AgentExperienceCollector):
             ongoing_next_obs (np.ndarray): The next observations for the agents.
         """
         ongoing_immediate_reward, ongoing_future_reward = get_ongoing_rewards_from_info(info, self.num_agents)
+        ongoing_immediate_reward = ongoing_reward - ongoing_future_reward
+        
         final_immediate_reward, final_future_reward = get_final_rewards_from_info(ongoing_terminated, info, self.num_agents)
 
         final_next_observation = np.zeros_like(ongoing_next_obs)