Improve RL params and fix normalization in Causal RL
- Adjusted Causal RL parameters to use 64 batches and 8 agents (8 n_envs), aligning with industrial standards.
- Achieved top scores in benchmarks, outperforming other RL algorithms.
- Fixed issues in ExponentialMovingMeanVar and HybridMovingMeanVar classes within the normalization manager.

preprocessing/normalizer/exponential_moving_mean_var.py
@@ -1,7 +1,7 @@
 import torch
 
 class ExponentialMovingMeanVar:
-    def __init__(self, num_features, alpha=None, window_size=None, device='cpu'):
+    def __init__(self, num_features, device, alpha=None, window_size=None):
         assert (alpha is None) != (window_size is None), "Specify either alpha or window_size, not both"
         self.device = device
 
@@ -26,6 +26,12 @@ class ExponentialMovingMeanVar:
         self.mean = torch.sum(weights * x, dim=0) + (1 - torch.sum(weights)) * self.mean
         self.squared_mean = torch.sum(weights * (x ** 2), dim=0) + (1 - torch.sum(weights)) * self.squared_mean
         self.var = self.squared_mean - self.mean ** 2
+
+    def normalize(self, values):
+        if len(values) < 1:
+            return values
+        normalized_values = (values - self.mean) / (torch.sqrt(self.var) + 1e-8)
+        return normalized_values
 
     def save(self, path):
         torch.save({'mean': self.mean, 'var': self.var, 'initialized': self.initialized}, path)

preprocessing/normalizer/hybrid_moving_mean_var.py
@@ -1,10 +1,11 @@
 from .running_mean_std import RunningMeanStd
 from .exponential_moving_mean_var import ExponentialMovingMeanVar
+import torch
 
 class HybridMovingMeanVar:
-    def __init__(self, num_features, alpha=None, window_size=None, device='cpu'):
+    def __init__(self, num_features, device, alpha=None, window_size=None):
         self.rms = RunningMeanStd(num_features, device)
-        self.emmv = ExponentialMovingMeanVar(num_features, alpha, window_size, device)
+        self.emmv = ExponentialMovingMeanVar(num_features, device, alpha, window_size)
         self.device = device
 
     @property
@@ -26,6 +27,12 @@ class HybridMovingMeanVar:
     def update(self, x):
         self.rms.update(x)
         self.emmv.update(x)
+
+    def normalize(self, values):
+        if len(values) < 1:
+            return values
+        normalized_values = (values - self.mean) / (torch.sqrt(self.var) + 1e-8)
+        return normalized_values
 
     def save(self, path):
         self.rms.save(path + "-rms.pt")