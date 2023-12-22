bd0d59a0ac8adb05512c519c2d20ba02c0c522d9
junho-deepdream <75417171+junho-deepdream@users.noreply.github.com>
2023-12-22 21:09:52 +0900

    Update base_trainer.py
    - Previously, advantages were scaled by the batch size, which preserved the scale of individual gradients within a batch. While this maintained consistent gradient magnitudes within each batch, it introduced variability in total gradient updates across different batch sizes.
    
    - The removal of this scaling is intended to stabilize gradient updates, especially when using varying batch sizes. Larger batches inadvertently led to more substantial updates, potentially causing rapid and unstable changes in the model parameters.

--- training/base_trainer.py
+++ training/base_trainer.py
@@ -68,8 +68,6 @@ class BaseTrainer(TrainingManager, NormalizationUtils, ExplorationUtils):
         If self.advantage_normalizer is not None, it uses the scale_advantage function to 
         apply a specific normalization technique to the advantages, potentially with thresholding.
         """
-            # Multiply by the batch size to scale the advantages
-        advantages = advantages.size(0) * advantages
         # Apply specific normalization to the advantages
         return scale_advantage(advantages, 
                             norm_type=self.advantage_normalizer, 
