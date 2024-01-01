Fixed Value Computation: Aligned with Model Sequence Length
Corrected a crucial value computation process in the base_trainer file, ensuring the target network's trajectory values align with the specified model_seq_length, without the additional length used in earlier versions. This adjustment resolves positional encoding inconsistencies in the Hugging Face GPT model, enhancing the framework's accuracy and efficiency by maintaining a uniform sequence length throughout the training process.

training/base_trainer.py
@@ -9,7 +9,7 @@ from utils.setting.rl_params import RLParameters
 from utils.structure.trajectories  import BatchTrajectory
 from .trainer_utils import calculate_gae_returns, calculate_lambda_returns, compute_discounted_future_value 
 from .trainer_utils import adaptive_masked_tensor_reduction, masked_tensor_reduction, apply_seq_mask, create_model_seq_mask
-from .trainer_utils import create_padding_mask_before_dones, convert_trajectory_data
+from .trainer_utils import create_padding_mask_before_dones
 
 class BaseTrainer(TrainingManager, NormalizationUtils, ExplorationUtils):
     def __init__(self, trainer_name, env_config: EnvConfig, rl_params: RLParameters, networks, target_networks, device):
@@ -63,11 +63,11 @@ class BaseTrainer(TrainingManager, NormalizationUtils, ExplorationUtils):
         model_seq_mask = create_model_seq_mask(padding_mask, model_seq_length)
 
         # Apply the mask to each trajectory component
-        sel_states = apply_seq_mask(states, model_seq_mask, model_seq_length)
-        sel_actions = apply_seq_mask(actions, model_seq_mask, model_seq_length)
-        sel_rewards = apply_seq_mask(rewards, model_seq_mask, model_seq_length)
-        sel_next_states = apply_seq_mask(next_states, model_seq_mask, model_seq_length)
-        sel_dones = apply_seq_mask(dones, model_seq_mask, model_seq_length)
+        sel_states = apply_seq_mask(states, model_seq_mask)
+        sel_actions = apply_seq_mask(actions, model_seq_mask)
+        sel_rewards = apply_seq_mask(rewards, model_seq_mask)
+        sel_next_states = apply_seq_mask(next_states, model_seq_mask)
+        sel_dones = apply_seq_mask(dones, model_seq_mask)
 
         return sel_states, sel_actions, sel_rewards, sel_next_states, sel_dones, model_seq_mask
 
@@ -110,14 +110,14 @@ class BaseTrainer(TrainingManager, NormalizationUtils, ExplorationUtils):
         states, actions, rewards, next_states, dones = trajectory 
         
         padding_mask = create_padding_mask_before_dones(dones)
-        trajectory_states, trajectory_mask = convert_trajectory_data(states, next_states, mask=padding_mask)
 
         mode_seq_sum_discounted_gammas = sum(self.discount_factors[:,-rewards.shape[1]:])
         scaled_rewards = rewards/mode_seq_sum_discounted_gammas
         
         with torch.no_grad():
             estimated_value = self.trainer_calculate_value_estimate(states, mask=padding_mask)
-            trajectory_values = self.trainer_calculate_future_value(trajectory_states, trajectory_mask)
+            future_values = self.trainer_calculate_future_value(next_states, padding_mask)
+            trajectory_values = torch.cat([estimated_value[:, :1], future_values], dim=1)
             
             if self.use_gae_advantage:
                 _advantage = calculate_gae_returns(trajectory_values, scaled_rewards, dones, self.discount_factor, self.advantage_lambda)
@@ -136,13 +136,13 @@ class BaseTrainer(TrainingManager, NormalizationUtils, ExplorationUtils):
         states, actions, rewards, next_states, dones = trajectory 
 
         padding_mask = create_padding_mask_before_dones(dones)
-        trajectory_states, trajectory_mask = convert_trajectory_data(states, next_states, mask=padding_mask)
 
         td_steps_sum_discounted_gammas = sum(self.discount_factors[:,-rewards.shape[1]:])
         scaled_rewards = rewards/td_steps_sum_discounted_gammas
         
         with torch.no_grad():
-            trajectory_values = self.trainer_calculate_future_value(trajectory_states, trajectory_mask)
+            future_values = self.trainer_calculate_future_value(next_states, padding_mask)
+            trajectory_values = torch.cat([estimated_value[:, :1], future_values], dim=1)
             
             if self.use_gae_advantage:
                 _advantage = calculate_gae_returns(trajectory_values, scaled_rewards, dones, self.discount_factor, self.advantage_lambda)
@@ -150,8 +150,7 @@ class BaseTrainer(TrainingManager, NormalizationUtils, ExplorationUtils):
             else:
                 _expected_value = calculate_lambda_returns(trajectory_values, scaled_rewards, dones, self.discount_factor, self.advantage_lambda)
             
-            model_seq_length = estimated_value.shape[1]
-            expected_value = apply_seq_mask(_expected_value, model_seq_mask, model_seq_length)
+            expected_value = apply_seq_mask(_expected_value, model_seq_mask)
             _advantage = (expected_value - estimated_value)
             
             advantage = self.normalize_advantage(_advantage)

training/trainer_utils.py
@@ -180,11 +180,6 @@ def scale_advantage(advantages, norm_type=None):
 
     return advantages / abs_mean_advantage
 
-def convert_trajectory_data(states, next_states, mask):
-    trajectory_states = torch.cat([states, next_states[:, -1:]], dim=1)
-    trajectory_mask = torch.cat([mask, mask[:, -1:]], dim=1)
-    return trajectory_states, trajectory_mask
-
 def create_model_seq_mask(padding_mask, model_seq_length):
     """
     Creates a selection mask for trajectories.
@@ -210,11 +205,10 @@ def create_model_seq_mask(padding_mask, model_seq_length):
     return select_mask
 
 # Function to apply selection mask to a trajectory component
-def apply_seq_mask(component, model_seq_mask, model_seq_length):
-    batch_size, seq_len, feature_size = component.shape
-    reshaped_mask = model_seq_mask.expand(-1, -1, feature_size).reshape(batch_size, -1)
-    selected = component.reshape(batch_size, -1)[reshaped_mask].view(batch_size, model_seq_length, feature_size)
-    return selected
+def apply_seq_mask(component, model_seq_mask):
+    model_seq_mask_shape = model_seq_mask.shape
+    component_shape = component.shape
+    return component[model_seq_mask.expand_as(component) > 0].reshape(component_shape[0], model_seq_mask_shape[1], component_shape[2])
 
 
 