24073dc954d627c86e00207b9d8a86d55ff78695
junho-deepdream <75417171+junho-deepdream@users.noreply.github.com>
2024-01-10 09:12:25 +0900

    Optimize Buffer Trajectory Slicing with Enhanced Indexing and Masking

--- memory/base_buffer.py
+++ memory/base_buffer.py
@@ -36,9 +36,6 @@ class BaseBuffer:
         expanded_indices = np.array([range(buffer_size + i -  td_steps + 1, buffer_size + i + 1) for i in indices]) % buffer_size
         expanded_indices = expanded_indices.reshape(batch_size, td_steps)
 
-        # Create a mask to identify valid indices within the current buffer size
-        valid_mask = expanded_indices < self.size if self.size < self.capacity else np.ones_like(expanded_indices, dtype=bool)
-                
         # Fetch the slices using advanced indexing
         states_slices = self.states[expanded_indices]
         actions_slices = self.actions[expanded_indices]
@@ -47,13 +44,17 @@ class BaseBuffer:
         terminated_slices = self.terminated[expanded_indices]
         truncated_slices = self.truncated[expanded_indices]
 
-        # Zero out the data at invalid indices
-        states_slices[~valid_mask] = 0
-        actions_slices[~valid_mask] = 0
-        rewards_slices[~valid_mask] = 0
-        next_states_slices[~valid_mask] = 0
-        terminated_slices[~valid_mask] = True
-        truncated_slices[~valid_mask] = True
+        if self.size < self.capacity:
+            # Create a mask to identify valid indices within the current buffer size
+            valid_mask = expanded_indices < self.size 
+                    
+            # Zero out the data at invalid indices
+            states_slices[~valid_mask] = 0
+            actions_slices[~valid_mask] = 0
+            rewards_slices[~valid_mask] = 0
+            next_states_slices[~valid_mask] = 0
+            terminated_slices[~valid_mask] = True
+            truncated_slices[~valid_mask] = True
 
         states_slices = states_slices.reshape(batch_size, td_steps, -1)
         actions_slices = actions_slices.reshape(batch_size, td_steps, -1)
