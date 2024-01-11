f15e8f00b7c1742a93e3f278f309492e7ebc7c39
KimJinSuAI <114320920+ccnets-team@users.noreply.github.com>
2024-01-11 14:00:49 +0900

    hot fix: function print_step

--- utils/printer.py
+++ utils/printer.py
@@ -44,7 +44,7 @@ def _print_metrics(label, **kwargs):
     
 
 def print_step(trainer, memory, episode, time_cost):
-    max_steps = trainer.max_steps
+    max_steps = trainer.exploration_params.max_steps
     if memory is not None:
         buffer_size = len(memory)
     else:
