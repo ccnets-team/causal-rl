37216cb73ba17990df18587f0cc1e1347ef29661
JeongYoong Kim <95277008+Ryoongyoong@users.noreply.github.com>
2024-01-16 16:47:56 +0900

    Hot fix: Replace next_observation with final_observation for truncated episodes

--- environments/gym_wrapper.py
+++ environments/gym_wrapper.py
@@ -134,8 +134,12 @@ class GymEnvWrapper(AgentExperienceCollector):
         np_reward = np.array(_reward, np.float32)
 
         np_done = np.logical_or(np_terminated, np_truncated)
-    
-        next_observation = get_final_observations_from_info(_info, np_next_obs)
+
+        next_observation = np_next_obs.copy()
+        for idx, trunc in enumerate(np_truncated):
+            if trunc:
+                next_observation[idx] = _info["final_observation"][idx]
+        
 
         if not self.test_env:
             self.update_agent_data(self.agents, self.observations[:, -1].to_vector(), action, np_reward, next_observation, np_terminated, np_truncated)
