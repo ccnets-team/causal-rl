Corrected Scaling Factor, EMA Weights https://wandb.ai/rl_tune/rl-tune-gym/
This commit introduces several important updates to our training process:

1. Adjusted Scaling Factor for Balanced Training: We have refined the calculation of scaling factors, which now involve dividing the sum of the product of gammas and lambdas by 2. This adjustment centers the sum of normalized rewards around the midpoint of the GPT sequence length, stabilizing value loss around 0.5 during training and facilitating smoother convergence.

2. Fixed EMA Weights Scaling Issue: We addressed a previous oversight in the scaling of EMA weights, where they were excessively reduced by dividing by the feature size. The revised scaling method ensures a more appropriate contribution of these weights to the EMA, enhancing its accuracy and relevance.

3. Transition to "running_mean_std" for State and Reward Normalization: In a strategic shift to simplify implementation, we've replaced the "exponential_moving_mean_var" normalizer with the "running_mean_std" normalizer for both state and reward normalization. This change streamlines the normalization process, making it more straightforward and easier to manage.

These enhancements collectively improve the stability, efficiency, and simplicity of our model training, leading to more consistent and reliable performance.

preprocessing/normalizer/exponential_moving_mean_var.py
@@ -40,7 +40,7 @@ class ExponentialMovingMeanVar:
 
         # Compute weights for each time step and expand them to match x's shape
         exponential_weights = self.alpha * ((1 - self.alpha) ** torch.arange(sequence_length-1, -1, -1, device=self.device))
-        exponential_weights = exponential_weights.view(1, sequence_length, 1).expand_as(x)/(batch_size * num_features)
+        exponential_weights = exponential_weights.view(1, sequence_length, 1).expand_as(x)/batch_size
 
         # Apply padding mask
         if padding_mask is not None:

training/base_trainer.py
@@ -19,7 +19,14 @@ class BaseTrainer(TrainingManager, NormalizationUtils, ExplorationUtils):
         self._init_exploration_utils()
         self.gammas = compute_discounted_future_value(self.discount_factor, self.max_seq_length)
         self.lambdas = compute_discounted_future_value(self.advantage_lambda, self.max_seq_length)
-        self.scaling_factors = torch.sum(self.gammas * self.lambdas, dim=1, keepdim=True).to(self.device)            
+
+        # Calculate the scaling factors by summing the product of gammas and lambdas across the sequence.
+        # The scaling factors are divided by 2 to scale the sum of normalized rewards to the midpoint of the GPT sequence length.
+        # This ensures that the cumulative effect of discounted rewards is centered around the average position in the sequence,
+        # effectively balancing the influence of rewards from the start to the end of the sequence.
+        # This scaling approach enables the value loss to be maintained around 0.5, preventing it from increasing or decreasing easily during training.
+        # Such stabilization is crucial for maintaining a consistent training process and achieving convergence.
+        self.scaling_factors = torch.sum(self.gammas * self.lambdas, dim=1, keepdim=True).to(self.device) / 2
         self.use_discrete = env_config.use_discrete
 
     def _unpack_rl_params(self, rl_params):