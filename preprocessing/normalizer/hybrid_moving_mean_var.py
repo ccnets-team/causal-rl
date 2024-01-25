bb3c607f18fac46b11bdbf20e37e488cb2d4473b
junho-deepdream <75417171+junho-deepdream@users.noreply.github.com>
2024-01-25 11:32:51 +0900

    sync: normalization from main branch

---
+++ preprocessing/normalizer/hybrid_moving_mean_var.py
@@ -0,0 +1,85 @@
+import torch
+
+class HybridMovingMeanVar:
+    def __init__(self, num_features, device, alpha=None, window_size=None):
+        assert (alpha is None or window_size is None), "Only one of alpha or window_size can be specified"
+        if window_size is not None:
+            alpha = 2 / (window_size + 1)
+        assert alpha is not None, "Either alpha or window_size must be specified"
+        assert 0 < alpha < 1, "alpha must be between 0 and 1 (exclusive)"
+        
+        self.alpha = alpha
+        self.window_size = window_size if window_size is not None else (2 / alpha - 1)
+        self.device = device
+        self.mean = torch.zeros(num_features, device=self.device, dtype=torch.float64)
+        self.squared_mean = torch.zeros(num_features, device=self.device, dtype=torch.float64)
+        self.var = torch.ones(num_features, device=self.device, dtype=torch.float64)
+        self.count = 0
+        self.initialized = False
+
+    def get_mean(self):
+        return self.mean
+
+    def get_var(self):
+        return self.var
+
+    def update(self, x, padding_mask=None):
+        batch_size, sequence_length, num_features = x.size()
+        if batch_size == 0:
+            return self  # Do not update if the batch size is zero
+
+        # Convert x to torch.float64 for high precision
+        x = x.to(dtype=torch.float64)
+        new_count = self.count + batch_size
+
+        if not self.initialized:
+            # Compute the mean and variance across both batch and sequence dimensions
+            self.mean = x.mean(dim=(0, 1))
+            self.squared_mean = (x ** 2).mean(dim=(0, 1))
+            self.var = x.var(dim=(0, 1), unbiased=False)
+            self.count = new_count
+            self.initialized = True
+            return
+
+        # Compute weights for each time step and expand them to match x's shape
+        exponential_weights = self.alpha * ((1 - self.alpha) ** torch.arange(sequence_length-1, -1, -1, device=self.device))
+        exponential_weights = exponential_weights.view(1, sequence_length, 1).expand_as(x)/batch_size
+
+        # Compute an adjustment factor based on batch size and total count
+        hybrid_weights = exponential_weights + (1 / new_count)
+        
+        sum_hybrid_weights = torch.sum(hybrid_weights, dim=(0, 1)).expand_as(hybrid_weights)
+        hybrid_weights = torch.where(sum_hybrid_weights > 1, hybrid_weights/sum_hybrid_weights, hybrid_weights)
+
+        # Apply padding mask
+        if padding_mask is not None:
+            weights = hybrid_weights * padding_mask.to(dtype=x.dtype)
+        else:
+            weights = hybrid_weights
+
+        self.mean = torch.sum(weights * x, dim=(0, 1)) + (1 - torch.sum(weights, dim=(0, 1))) * self.mean
+        self.squared_mean = torch.sum(weights * (x**2), dim=(0, 1)) + (1 - torch.sum(weights, dim=(0, 1))) * self.squared_mean
+        self.var = self.squared_mean - self.mean**2
+        self.count = new_count
+
+    def normalize(self, x):
+        if len(x) < 1 or not self.initialized:
+            return x
+        mean = self.get_mean().view(1, 1, -1)
+        var = self.get_var().view(1, 1, -1)
+        normalized_x = (x - mean) / (torch.sqrt(var) + 1e-8)
+        return normalized_x.to(dtype=x.dtype)
+
+    def save(self, path):
+        torch.save({'mean': self.mean, 'squared_mean': self.squared_mean, 'var': self.var, \
+            'count': self.count, 'alpha': self.alpha, 'window_size': self.window_size, 'initialized': self.initialized}, path)
+
+    def load(self, path):
+        checkpoint = torch.load(path, map_location=self.device)
+        self.mean = checkpoint['mean']
+        self.squared_mean = checkpoint['squared_mean']
+        self.var = checkpoint['var']
+        self.alpha = checkpoint['alpha']
+        self.window_size = checkpoint['window_size']
+        self.initialized = checkpoint['initialized']
+        self.count = checkpoint['count']
--- training/managers/normalization_manager.py
+++ training/managers/normalization_manager.py
@@ -1,7 +1,11 @@
 import torch
 from preprocessing.normalizer.running_mean_std import RunningMeanStd
 from preprocessing.normalizer.exponential_moving_mean_var import ExponentialMovingMeanVar
+from preprocessing.normalizer.hybrid_moving_mean_var import HybridMovingMeanVar
+
+import numpy as np
 from utils.structure.trajectories  import BatchTrajectory
+from training.trainer_utils import create_padding_mask_before_dones
 
 TRANSITION_STATE_IDX = 0
 TRANSITION_NEXT_STATE_IDX = 3
@@ -11,21 +15,23 @@ EXPONENTIAL_MOVING_ALPHA = 1e-4
 CLIP_NORM_RANGE = 10.0
 
 class NormalizerBase:
-    def __init__(self, vector_size, norm_type_key, normalization_params, device):
+    def __init__(self, feature_size, norm_type_key, normalization_params, device):
         self.normalizer = None
-        self.vector_size = vector_size
+        self.feature_size = feature_size
         self.exponential_moving_alpha = EXPONENTIAL_MOVING_ALPHA  # Alpha value for exponential moving average, used in 'exponential_moving_mean_var' normalizer to determine weighting of recent data.
         self.clip_norm_range = CLIP_NORM_RANGE  # The range within which normalized values are clipped, preventing excessively high normalization values.
 
         norm_type = getattr(normalization_params, norm_type_key)
         if norm_type == "running_mean_std":
-            self.normalizer = RunningMeanStd(vector_size, device)
+            self.normalizer = RunningMeanStd(feature_size, device)
         elif norm_type == "exponential_moving_mean_var":
-            self.normalizer = ExponentialMovingMeanVar(vector_size, device, alpha = self.exponential_moving_alpha)
+            self.normalizer = ExponentialMovingMeanVar(feature_size, device, alpha = self.exponential_moving_alpha)
+        elif norm_type == "hybrid_moving_mean_var":
+            self.normalizer = HybridMovingMeanVar(feature_size, device, alpha = self.exponential_moving_alpha)
             
         self.device = device
                     
-    def _update_normalizer(self, data):
+    def _update_normalizer(self, data, padding_mask=None):
         if self.normalizer is not None:
             # Convert to a PyTorch tensor if it's a NumPy array and move to the specified device
             if not isinstance(data, torch.Tensor):
@@ -33,7 +39,7 @@ class NormalizerBase:
             elif data.device != self.device:
                 data = data.to(self.device)
             # Update the normalizer with the reshaped data
-            self.normalizer.update(data)
+            self.normalizer.update(data, padding_mask)
                 
     def _normalize_data(self, data):
         if self.normalizer is not None:
@@ -68,13 +74,13 @@ class NormalizationUtils:
     def normalize_reward(self, reward):
         return self.reward_manager._normalize_data(reward)
 
-    def normalize_advantage(self, advantage):
+    def normalize_advantage(self, advantage, padding_mask=None):
         """Normalize the returns based on the specified normalizer type."""
         if self.advantage_normalizer is None:
             normalized_advantage = advantage
         else:
             _advantage = advantage.squeeze(-1).unsqueeze(1)
-            self.advantage_manager._update_normalizer(_advantage)
+            self.advantage_manager._update_normalizer(_advantage, padding_mask)
             _normalized_advantage = self.advantage_manager._normalize_data(_advantage)
             normalized_advantage = _normalized_advantage.squeeze(1).unsqueeze(-1)
         return normalized_advantage
@@ -87,7 +93,7 @@ class NormalizationUtils:
 
     def update_normalizer(self, trajectories: BatchTrajectory):
         states, actions, rewards, next_states, dones = trajectories
-
+        padding_mask = create_padding_mask_before_dones(dones)
         # Update state normalizer
         for index in self.state_indices:
             if index == TRANSITION_STATE_IDX:
@@ -96,7 +102,7 @@ class NormalizationUtils:
                 state_data = next_states
             else:
                 raise ValueError("Invalid state index")
-            self.state_manager._update_normalizer(state_data)
+            self.state_manager._update_normalizer(state_data, padding_mask)
 
         # Update reward normalizer
         for index in self.reward_indices:
@@ -104,4 +110,4 @@ class NormalizationUtils:
                 reward_data = rewards
             else:
                 raise ValueError("Invalid reward index")
-            self.reward_manager._update_normalizer(reward_data)
+            self.reward_manager._update_normalizer(reward_data, padding_mask)
