3bb8748e7578999ef156413d4b36aaf0c62026b9
junho-deepdream <75417171+junho-deepdream@users.noreply.github.com>
2024-01-26 18:32:19 +0900

    change "batch_norm" advantage normalizion without batch size multiplication https://wandb.ai/rl_tune/rl-tune-gym/

--- training/managers/normalization_manager.py
+++ training/managers/normalization_manager.py
@@ -100,7 +100,7 @@ class NormalizationUtils:
             # Batch normalization - normalizing based on batch mean and std
             batch_mean_estimated = advantage.mean(dim=0, keepdim=True)
             batch_std_estimated = advantage.std(dim=0, keepdim=True) + 1e-8
-            normalized_advantage = advantage.size(0) * (advantage - batch_mean_estimated) / batch_std_estimated
+            normalized_advantage = (advantage - batch_mean_estimated) / batch_std_estimated
         else:
             reshaped_advantage = advantage.squeeze(-1).unsqueeze(1)
             self.advantage_manager._update_normalizer(reshaped_advantage, padding_mask)
