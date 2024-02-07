fix(reward_scaling): Remove post-normalized_sum_reward scaling
This commit removes the post-normalization scaling operation previously applied after the normalized_sum_reward calculation.

Extensive experimentation has indicated that the additional scaling factors introduced do not contribute meaningful improvements to the model's performance.

Key changes:
- Eliminate additional scaling operations following normalized_sum_reward calculation.
- Simplify the reward scaling process by relying solely on initial normalization techniques.

training/base_trainer.py
@@ -6,7 +6,7 @@ from abc import abstractmethod
 from utils.structure.env_config import EnvConfig
 from utils.setting.rl_params import RLParameters
 from utils.structure.data_structures  import BatchTrajectory
-from .trainer_utils import calculate_lambda_returns, masked_tensor_reduction, create_sum_reward_scale
+from .trainer_utils import calculate_lambda_returns, masked_tensor_reduction
 
 class BaseTrainer(TrainingManager, NormalizationUtils, ExplorationUtils):
     def __init__(self, env_config: EnvConfig, rl_params: RLParameters, networks, target_networks, device):
@@ -15,7 +15,6 @@ class BaseTrainer(TrainingManager, NormalizationUtils, ExplorationUtils):
         self._init_training_manager(networks, target_networks, device)
         self._init_normalization_utils(env_config, device)
         self._init_exploration_utils(rl_params.max_steps)
-        self.sum_reward_scale = create_sum_reward_scale(self.discount_factor, self.advantage_lambda, self.gpt_seq_length, self.device)
 
     def _unpack_rl_params(self, rl_params):
         (self.training_params, self.algorithm_params, self.network_params, 
@@ -87,7 +86,7 @@ class BaseTrainer(TrainingManager, NormalizationUtils, ExplorationUtils):
             future_values = self.trainer_calculate_future_value(next_states, padding_mask)
             trajectory_values = torch.cat([torch.zeros_like(future_values[:, :1]), future_values], dim=1)
             expected_value, sum_rewards = calculate_lambda_returns(trajectory_values, rewards, dones, self.discount_factor, self.advantage_lambda)
-            normalized_sum_rewards = self.normalize_sum_rewards(sum_rewards, padding_mask, self.sum_reward_scale)
+            normalized_sum_rewards = self.normalize_sum_rewards(sum_rewards, padding_mask)
             expected_value = expected_value - sum_rewards + normalized_sum_rewards
             
         with torch.no_grad():