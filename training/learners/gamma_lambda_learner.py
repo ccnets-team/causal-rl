Fix gradient issue on raw_lambd reset and refine sequence length calculation
- Modified the `calculate_learnable_sequence_length` function to improve the logic for determining the optimal sequence length for training. The function now uses updated `contraction_relevance_threshold` and `extension_relevance_threshold` parameters for more accurate adjustments based on the analysis of the lambda-derived probability distribution.
- Addressed a critical issue where resetting `raw_lambd` was causing gradients to not be properly tracked. The lambda values are now updated in-place, ensuring that gradient computation and optimization continue seamlessly after the reset.
- Implemented a conditional initialization of `input_seq_len` to half of `max_seq_len` (`init_half_seq_len`) when `auto_lambda_init` is set to False, providing a more sensible starting point for sequence length in scenarios where automatic lambda initialization is not utilized.

training/learners/gamma_lambda_learner.py
@@ -42,7 +42,7 @@ class GammaLambdaLearner(nn.Module):
             new_raw_lambdas = new_lambdas
 
         # Update the raw lambda parameter
-        self.raw_lambd = nn.Parameter(new_raw_lambdas)
+        self.raw_lambd.data = new_raw_lambdas
                 
     def save(self, path):
         torch.save({'raw_gamma': self.raw_gamma, 'raw_lambd': self.raw_lambd}, path)

training/learners/sequence_length_learner.py
@@ -13,6 +13,7 @@ class SequenceLengthLearner:
         # Flags to track the initialization status
         self.init_lambda_first_dist = False if use_auto_init_lambdas else True 
         self.init_lambda_second_dist = False if use_auto_init_lambdas else True 
+        self.init_half_seq_len = True if use_auto_init_lambdas else False 
         
         # Initialize TD extension steps
         self._update_td_extension_steps()
@@ -34,9 +35,12 @@ class SequenceLengthLearner:
             else:
                 self.second_distribution = create_prob_dist_from_lambdas(lambda_values)
                 initial_length = torch.argmax(self.second_distribution - self.first_distribution, dim=0).long().item() + 1
-                self.gamma_lambda_learner_for_seq.reset_lambdas(start_id = initial_length)
+                self.gamma_lambda_learner_for_seq.reset_lambdas(start_idx = initial_length)
                 self.input_seq_len = min(max(initial_length, self.min_seq_len), self.max_seq_len)
                 self.init_lambda_second_dist = True
+        elif not self.init_half_seq_len:
+            self.input_seq_len = self.max_seq_len // 2
+            self.init_half_seq_len = True
         else:
             optimal_length = calculate_learnable_sequence_length(lambda_values)
             self.input_seq_len = min(max(optimal_length, self.min_seq_len), self.max_seq_len)